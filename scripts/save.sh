327ef975704c5c7caf0ea134df8f2097bbed8e27
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-05-08 07:30:00 -0400

    Print file location that is being saved

--- scripts/save.sh
+++ scripts/save.sh
@@ -266,7 +266,7 @@ show_output() {
 main() {
 	if supported_tmux_version_ok; then
 		if show_output; then
-			start_spinner "Saving..." "Tmux environment saved!"
+			start_spinner "Saving to $(resurrect_file_path) ..." "Tmux environment saved!"
 		fi
 		save_all
 		if show_output; then
